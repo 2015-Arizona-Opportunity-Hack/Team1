0485984458cd8ff16aeb46f09765b28babd4aa23
Dan <JamesDanielBaird@gmail.com>
2015-10-10 13:58:16 -0700

    basic make post method

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
+client = MongoClient()
+db = client['core-db']
 
 from views import *
